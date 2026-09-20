70ac332157ba8a1d1fa64c26948935ba99b016c2
baicuovidiu <baicu.ovi@gmail.com>
2026-09-20 08:00:43 +0300

    Motor 2.0: define native compositor draw ABI

--- src/Motor2.Native/include/motor2_native.h
+++ src/Motor2.Native/include/motor2_native.h
@@ -19,3 +19,15 @@ MOTOR2_API void motor2_d3d12_destroy(void* context);
 MOTOR2_API void* motor2_d3d12_create_texture_rgba8(void* context, std::uint32_t width, std::uint32_t height);
 MOTOR2_API int motor2_d3d12_upload_rgba8(void* context, void* texture, const void* pixels, std::uint32_t rowPitch, std::uint32_t height);
 MOTOR2_API void motor2_d3d12_release_resource(void* resource);
+
+struct Motor2DrawQuad {
+    void* texture;
+    float m11, m12, m21, m22, m31, m32;
+    float opacity;
+    float z;
+};
+
+MOTOR2_API void* motor2_d3d12_create_render_target(void* context, std::uint32_t width, std::uint32_t height);
+MOTOR2_API int motor2_d3d12_begin_frame(void* context, void* target);
+MOTOR2_API int motor2_d3d12_draw_quads(void* context, void* target, const Motor2DrawQuad* commands, std::uint32_t count);
+MOTOR2_API int motor2_d3d12_end_frame(void* context, void* target);
