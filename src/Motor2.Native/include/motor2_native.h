309f67dc0db40479199ad510fc1170d765a69151
baicuovidiu <baicu.ovi@gmail.com>
2026-09-20 08:41:02 +0300

    Motor 2.0: make GPU resource release context-aware and type-safe

--- src/Motor2.Native/include/motor2_native.h
+++ src/Motor2.Native/include/motor2_native.h
@@ -18,7 +18,8 @@ MOTOR2_API void* motor2_d3d12_create();
 MOTOR2_API void motor2_d3d12_destroy(void* context);
 MOTOR2_API void* motor2_d3d12_create_texture_rgba8(void* context, std::uint32_t width, std::uint32_t height);
 MOTOR2_API int motor2_d3d12_upload_rgba8(void* context, void* texture, const void* pixels, std::uint32_t rowPitch, std::uint32_t height);
-MOTOR2_API void motor2_d3d12_release_resource(void* resource);
+MOTOR2_API void motor2_d3d12_release_resource(void* context, void* resource);
+MOTOR2_API void motor2_d3d12_release_render_target(void* context, void* target);
 
 struct Motor2DrawQuad {
     void* texture;
